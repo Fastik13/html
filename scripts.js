784e25a8d7e29bbe0007cc701cdace4ba457c94c
Fastik13 <ilyatitan4@gmail.com>
2022-03-28 12:56:09 +0300

    wwwf

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const dino = document .getElementById("dino")
+const cactus =document .getElementById("cactus")
+
+document .addEventListener("keydown", function(event) {
+jump()
+})
+
+function jump () {
+ if (dino.classList !="jump"){   
+dino.classList.add("jump")     
+}
+setTimeout( function(){
+dino.classList.remove("jump")
+},300)
+
+}
+
+let isAlive = setInterval (function(){
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+if (cactusleft <50 && cactusleft > 0 && dinoTop >= 140) {
+   alert("GAME OVER!!") 
+   
+        
+                                                                                                        
+}
+    }, 10)
